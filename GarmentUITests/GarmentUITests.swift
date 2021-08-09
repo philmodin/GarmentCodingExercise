8694d2b8987a76237a7ac491c44dbaf1c3b60eae
Philip Modin <18331871+philmodin@users.noreply.github.com>
2021-08-09 12:00:02 -0700

    Cleaned up comments and file names.

--- GarmentUITests/GarmentUITests.swift
+++ GarmentUITests/GarmentUITests.swift
@@ -8,15 +8,12 @@
 import XCTest
 
 class GarmentUITests: XCTestCase {
-
-    let app = XCUIApplication()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -26,8 +23,8 @@ class GarmentUITests: XCTestCase {
 
     func testAddGarment() throws {
         // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
+        let app = XCUIApplication()
+        app.launch()
 
         let addGarmentButton = app.buttons["addGarmentButton"]
         addGarmentButton.tap()
